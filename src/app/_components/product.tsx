d2835b5322c00c57e7ada4f03930f4e27049a6f5
Boona06 <bb851913@gmail.com>
2025-03-28 19:46:46 +0800

    some bug

---
+++ src/app/_components/product.tsx
@@ -0,0 +1,21 @@
+export function ProductCard(){
+return(
+ <div className="flex ">
+ <div className="w-40 h-40px bg-[#f1f1f1] m-auto mt-10 rounded-2xl">
+         <h2 className="text-[#ab3030]">Aaviin Baraa</h2>
+         <h3 className="text-black">₮100'000</h3>
+         <h5 className="text-[#5dc477]">Бэлэн</h5>
+    </div>
+    <div className="w-xs h-[500px] bg-[#f1f1f1] m-auto mt-10 rounded-2xl">
+    <h2 className="text-[#ab3030]">Aaviin Baraa</h2>
+    <h3 className="text-black">₮100'000</h3>
+    <h5 className="text-[#5dc477]">Бэлэн</h5>
+</div>
+<div className="w-xs h-[500px] bg-[#f1f1f1] m-auto mt-10 rounded-2xl">
+    <h2 className="text-[#ab3030]">Aaviin Baraa</h2>
+    <h3 className="text-black">₮100'000</h3>
+    <h5 className="text-[#5dc477]">Бэлэн</h5>
+</div>
+ </div>
+    
+)}
