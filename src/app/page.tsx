c7adbba530b59dc1e47ad0ccc452c1fcef59f453
Boona06 <bb851913@gmail.com>
2025-03-29 16:03:00 +0800

    some bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,12 @@ import ProductCard from "./_components/product";
 
 export default function Home() {
   return (
-    <div className="mt-44 ml-60 gap-3 flex">
+    <div className="mt-44 ml-60 gap-3 grid grid-cols-2">
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
       <ProductCard />
       <ProductCard />
     </div>
