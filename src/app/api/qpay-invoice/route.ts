cec16cb0ea1304478cb0d65ef28c5772fd6c6c65
Munkhtsolmon2222 <monkhtsolmonmuzo@gmail.com>
2025-04-04 20:49:15 +0800

    nothing

--- src/app/api/qpay-invoice/route.ts
+++ src/app/api/qpay-invoice/route.ts
@@ -1,6 +1,6 @@
 import { sendEmail } from "@/lib/sendEmail";
 import { NextRequest } from "next/server";
-
+ 
 export async function POST(req: NextRequest) {
   try {
     const {
@@ -11,7 +11,7 @@ export async function POST(req: NextRequest) {
       callback_url,
       name,
     } = await req.json();
-
+ 
     const response = await fetch("https://merchant.qpay.mn/v2/invoice", {
       method: "POST",
       headers: {
@@ -27,35 +27,35 @@ export async function POST(req: NextRequest) {
         callback_url,
       }),
     });
-
+ 
     if (!response.ok) {
       const errorText = await response.text();
       return new Response(errorText, { status: response.status });
     }
-
+ 
     // Get the email cookie and decode it
     const emailCookie = req.cookies.get("clientEmail")?.value;
     if (!emailCookie) {
       return new Response("Email cookie is missing", { status: 400 });
     }
-
+ 
     // Decode the email from the cookie (URL-decoding)
     const email = decodeURIComponent(emailCookie);
     if (!email || !email.includes("@")) {
       return new Response("Invalid email address", { status: 400 });
     }
-
+ 
     // Send email
     await sendEmail({
       to: email,
       subject: "Invoice Confirmation",
       html: `
-        <h3>Сайн байна уу, ${name}!</h3>
+        <h3>Сайн байна уу, Эрчүүдийн дэлгүүр !</h3>
         <p>Таны <strong>${amount}₮</strong>-ийн худалдан авалтын нэхэмжлэл амжилттай үүсгэгдлээ.</p>
         <p>Баярлалаа!</p>
       `,
     });
-
+ 
     const data = await response.json();
     return new Response(JSON.stringify(data), { status: 200 });
   } catch (error) {
@@ -63,3 +63,5 @@ export async function POST(req: NextRequest) {
     return new Response("Server Error", { status: 500 });
   }
 }
+ 
+ 
